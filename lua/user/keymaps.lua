3b0c1a53971255e72a64464c4a55681d2d8843fb
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2022-07-09 15:13:18 +0200

    chore: backup progress

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -16,17 +16,17 @@ vim.g.mapleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- -- Better window navigation
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-w>k", opts)
+-- keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-w><Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-w><Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-w><Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-w><Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
