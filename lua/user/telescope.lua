e3ee6d034582c4b4acf02356c6a4d4c775315d1f
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2022-07-26 14:45:49 +0200

    feat: adding fzf plugin for telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -12,7 +12,16 @@ telescope.setup {
     selection_caret = " ",
     path_display = { "smart" },
     file_ignore_patterns = { ".git/", "node_modules" },
+    extensions = {
+      fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = true,  -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                         -- the default case_mode is "smart_case"
+      }
 
+    },
     mappings = {
       i = {
         ["<Down>"] = actions.cycle_history_next,
@@ -23,3 +32,5 @@ telescope.setup {
     },
   },
 }
+
+telescope.load_extension('fzf')
