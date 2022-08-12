4a9be6fd049e68a1cdfde914b255dd33f57219c4
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2022-08-12 14:43:40 +0200

    fix: formatting shortcut

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -147,7 +147,7 @@ local mappings = {
 			"<cmd>Telescope lsp_workspace_diagnostics<cr>",
 			"Workspace Diagnostics",
 		},
-		f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
+		f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
 		h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
 		i = { "<cmd>LspInfo<cr>", "Info" },
 		I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
