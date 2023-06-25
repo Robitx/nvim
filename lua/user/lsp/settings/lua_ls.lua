35981b51b1710cff1f67b65672a30b057d883584
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-06-25 13:55:38 +0200

    chore: update lua_ls settings

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -1,18 +1,20 @@
 return {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.stdpath "config" .. "/lua"] = true,
-        },
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+					-- Make the server aware of Neovim runtime files
+					vim.api.nvim_get_runtime_file("", true),
+				},
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
