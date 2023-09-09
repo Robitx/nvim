503a9a2593ed00231952084c71870c30b1c71649
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-09-09 16:34:15 +0200

    chore: treesitter deps for blankline

--- lua/user/plugins/indentline.lua
+++ lua/user/plugins/indentline.lua
@@ -1,19 +1,23 @@
-local config = function ()
-    require("indent_blankline").setup {
-      char = "▏",
-      show_trailing_blankline_indent = false,
-      show_first_indent_level = true,
-      use_treesitter = true,
-      show_current_context = true,
-      buftype_exclude = { "terminal", "nofile" },
-      filetype_exclude = {
-        "help",
-        "packer",
-        "NvimTree",
-      },
-    }
+local config = function()
+	require("indent_blankline").setup({
+		char = "▏",
+		show_trailing_blankline_indent = false,
+		show_first_indent_level = true,
+		use_treesitter = true,
+		show_current_context = true,
+		buftype_exclude = { "terminal", "nofile" },
+		filetype_exclude = {
+			"help",
+			"packer",
+			"NvimTree",
+		},
+	})
 end
 
 return {
-	{ "lukas-reineke/indent-blankline.nvim", config = config },
+	"lukas-reineke/indent-blankline.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = config,
 }
