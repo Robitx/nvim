2aa73e79560ee4a5aebb943e47a9f3ebd240101a
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-11-07 22:15:38 +0100

    chore: luacheck linter vim global

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -16,6 +16,10 @@ return {
 	config = function()
 		require("lint").linters_by_ft = linters
 
+		require("lint").linters.luacheck.args = {
+			args = { "--globals", "vim" },
+		}
+
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
 				require("lint").try_lint()
