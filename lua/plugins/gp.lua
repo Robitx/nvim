83686822d9890ac7cc7438d55303c6ef708626dd
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-11-29 20:27:25 +0100

    chore: gp conf update

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -4,8 +4,30 @@ local config = {
 	-- explicitly confirm deletion of a chat file
 	chat_confirm_delete = false,
 
-	chat_model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
-	command_model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
+	hooks = {
+		UnitTests = function(gp, params)
+			local template = "I have the following code from {{filename}}:\n\n"
+				.. "```{{filetype}}\n{{selection}}\n```\n\n"
+				.. "Please respond by writing table driven unit tests for the code above."
+			gp.Prompt(params, gp.Target.enew, nil, nil, template, gp.config.command_system_prompt)
+		end,
+
+		Translator = function(gp, params)
+			local chat_system_prompt = "You are a Translator, help me translate between English and Chinese."
+			gp.cmd.ChatNew(params, nil, chat_system_prompt)
+		end,
+
+		ProofReader = function(gp, params)
+			local chat_system_prompt = "I want you act as a proofreader. I will"
+				.. "provide you texts and I would like you to review them for any"
+				.. "spelling, grammar, or punctuation errors. Once you have finished"
+				.. "reviewing the text, provide me with any necessary corrections or"
+				.. "suggestions for improve the text. Highlight the corrections with"
+				.. "markdown bold or italics style."
+			gp.cmd.ChatNew(params, nil, chat_system_prompt)
+		end,
+	},
+
 }
 
 local register_shortcuts = function()
@@ -16,7 +38,7 @@ local register_shortcuts = function()
 		["<C-g>"] = {
 			c = { ":<C-u>'<,'>GpChatNew<cr>", "Visual Chat New" },
 			v = { ":<C-u>'<,'>GpChatPaste<cr>", "Visual Chat Paste" },
-			t = { ":<C-u>'<,'>GpChatToggle<cr>", "Visual Popup Chat" },
+			t = { ":<C-u>'<,'>GpChatToggle<cr>", "Visual Toggle Chat" },
 
 			["<C-x>"] = { ":'<,'>GpChatNew split<CR>", "Visual Chat New split" },
 			["<C-v>"] = { ":'<,'>GpChatNew vsplit<CR>", "Visual Chat New vsplit" },
@@ -27,6 +49,10 @@ local register_shortcuts = function()
 			b = { ":<C-u>'<,'>GpPrepend<cr>", "Visual Prepend" },
 			e = { ":<C-u>'<,'>GpEnew<cr>", "Visual Enew" },
 			p = { ":<C-u>'<,'>GpPopup<cr>", "Visual Popup" },
+
+			x = { ":<C-u>'<,'>GpContext<cr>", "Visual Toggle Context" },
+
+			n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
 			s = { "<cmd>GpStop<cr>", "Stop" },
 
 			-- optional Whisper commands
@@ -52,7 +78,7 @@ local register_shortcuts = function()
 		-- ...
 		["<C-g>"] = {
 			c = { "<cmd>GpChatNew<cr>", "New Chat" },
-			t = { "<cmd>GpChatToggle<cr>", "Toggle Popup Chat" },
+			t = { "<cmd>GpChatToggle<cr>", "Toggle Chat" },
 			f = { "<cmd>GpChatFinder<cr>", "Chat Finder" },
 
 			["<C-x>"] = { "<cmd>GpChatNew split<cr>", "New Chat split" },
@@ -64,6 +90,9 @@ local register_shortcuts = function()
 			b = { "<cmd>GpPrepend<cr>", "Prepend" },
 			e = { "<cmd>GpEnew<cr>", "Enew" },
 			p = { "<cmd>GpPopup<cr>", "Popup" },
+
+			x = { "<cmd>GpContext<cr>", "Toggle Context" },
+			n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
 			s = { "<cmd>GpStop<cr>", "Stop" },
 
 			-- optional Whisper commands
@@ -101,6 +130,9 @@ local register_shortcuts = function()
 			b = { "<cmd>GpPrepend<cr>", "Prepend" },
 			e = { "<cmd>GpEnew<cr>", "Enew" },
 			p = { "<cmd>GpPopup<cr>", "Popup" },
+
+			x = { "<cmd>GpContext<cr>", "Toggle Context" },
+			n = { "<cmd>GpNextAgent<cr>", "Next Agent" },
 			s = { "<cmd>GpStop<cr>", "Stop" },
 
 			-- optional Whisper commands
@@ -125,6 +157,7 @@ end
 return {
 	"robitx/gp.nvim",
 	dev = not os.getenv("RUNNING_IN_DOCKER"),
+	-- version = "v1.13.0",
 
 	config = function()
 		require("gp").setup(config)
