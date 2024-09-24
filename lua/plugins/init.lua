69afdab6edd05a84b70a725675453934ae1be028
Tibor Schmidt <robitx@gmail.com>
2024-09-24 14:53:18 +0200

    chore: updates

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,9 +25,9 @@ return {
 	},
 
 	{
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-		config = function()
+		"smoka7/hop.nvim",
+		confiversion = "*",
+		g = function()
 			-- you can configure Hop the way you like here; see :h hop-config
 			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 		end,
